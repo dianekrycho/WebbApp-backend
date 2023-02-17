ef4501396ed5d1fb085cd5be9de91fb261d9609c
dianekrycho <diane.krychowski@gmail.com>
2023-02-17 14:25:21 +0100

    cors error

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "");
   res.setHeader("Access-Control-Allow-Headers", "*");
-  //res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
   next();
 });
 app.use(bodyParser.json());
